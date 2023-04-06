f7d04462d16901b16f9cbaedc834499b5ec12653
Javier <javierabollado@gmail.com>
2023-04-06 18:32:38 +0200

    Añadir gráficos para la rotación de la imagen

--- main.py
+++ main.py
@@ -9,21 +9,6 @@ from skimage import io, color
 #os.chdir()
 
 
-"""
-Ejemplo para el apartado 1.
-
-Modifica la figura 3D y/o cambia el color
-https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
-"""
-
-
-"""
-Transformación para el segundo apartado
-
-NOTA: Para el primer aparado es necesario adaptar la función o crear otra similar
-pero teniendo en cuenta más dimensiones
-"""
-
 # Matriz de rotación
 M = lambda theta : np.array([[np.cos(theta), -np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
 M2 = lambda theta : np.array([[[np.cos(theta)]*3, [-np.sin(theta)]*3, [0]*3], [[np.sin(theta)]*3, [np.cos(theta)]*3, [0]*3], [[0]*3, [0]*3, [1]*3]])
@@ -45,7 +30,7 @@ def Traslacion(X0, Y0, Z0, t):
     X, Y, Z = a*t+X0, b*t + Y0, c*t+Z0
     return X, Y, Z
 
-def plot_frame(t, X0, Y0, Z0, theta, traslacion, ax):
+def plot_frame_1(t, X0, Y0, Z0, theta, traslacion, ax):
     # matrix = M(theta[t])
     # X, Y, Z = Rotacion_M(X0, Y0, Z0, matrix)
     X, Y, Z = Rotacion(X0, Y0, Z0, theta[t])
@@ -57,13 +42,13 @@ def plot_frame(t, X0, Y0, Z0, theta, traslacion, ax):
     cset = ax.contour(X, Y, Z, 16, extend3d=True, cmap=plt.cm.get_cmap('viridis'))
     ax.clabel(cset, fontsize=9, inline=1)
 
-def create_animation(X0, Y0, Z0, theta, traslacion):
+def create_animation_1(X0, Y0, Z0, theta, traslacion):
     N = len(theta) # = len(traslacion)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.view_init(elev=20, azim=-45)
     ts = range(N) 
-    ani = animation.FuncAnimation(fig, plot_frame, ts, fargs=[X0,Y0,Z0,theta, traslacion, ax], interval=10)
+    ani = animation.FuncAnimation(fig, plot_frame_1, ts, fargs=[X0,Y0,Z0,theta, traslacion, ax], interval=10)
     plt.show()
 
 
@@ -75,9 +60,28 @@ def problema1():
     d = Z.max()-Z.min()  # diámetro mayor
     theta = np.linspace(0, 3*np.pi, N)
     traslacion = np.linspace(0, d, N)
-    create_animation(X, Y, Z, theta, traslacion)
+    create_animation_1(X, Y, Z, theta, traslacion)
 
+##########################          PROBLEMA 2          ##########################
 
+def get_xyz(img):
+
+    # xx, yy -> coordenadas
+    # zz -> imagen (verde)
+    x = np.arange(0, img.shape[0], 1)
+    y = np.arange(0, img.shape[1], 1)
+    xx, yy = np.meshgrid(x, y)
+    xx = np.asarray(xx).reshape(-1)
+    yy = np.asarray(yy).reshape(-1)
+    z  = img[:,:,1]
+    zz = np.asarray(z).reshape(-1)
+
+    # Consideraremos sólo los elementos con zz < 240
+    x0 = xx[zz<240]
+    y0 = yy[zz<240]
+    z0 = zz[zz<240]/256. # -> normalizar los datos entre 0, 1.
+
+    return x0, y0, z0
 
 def transf1D(x,y,z,M, v=np.array([0,0,0])):
     xt = x*0
@@ -88,84 +92,38 @@ def transf1D(x,y,z,M, v=np.array([0,0,0])):
         xt[i], yt[i], zt[i] = np.matmul(M, q) + v
     return xt, yt, zt
 
-
-
-"""
-Segundo apartado casi regalado
-
-Imagen del árbol
-"""
-
-#os.getcwd()
-#os.chdir()
-
-img = io.imread('arbol.png')
-#dimensions = color.guess_spatial_dimensions(img)
-#print(dimensions)
-#io.show()
-#io.imsave('arbol2.png',img)
-
-#https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
-fig = plt.figure(figsize=(5,5))
-for i in range(3):
-    ax = fig.add_subplot(1, 3, i+1)
-    ax.set_title(f"img[:,:,{i}]")
-    ax.contourf(img[:,:,i], cmap = plt.cm.get_cmap('viridis'), levels=np.arange(0,240,2))
-    ax.axis('off')
-#fig.colorbar(p)
-
-xyz = img.shape
-
-x = np.arange(0,xyz[0],1)
-y = np.arange(0,xyz[1],1)
-xx,yy = np.meshgrid(x, y)
-xx = np.asarray(xx).reshape(-1)
-yy = np.asarray(yy).reshape(-1)
-z  = img[:,:,1]
-zz = np.asarray(z).reshape(-1)
-
-
-"""
-Consideraremos sólo los elementos con zz < 240 
-
-Por curiosidad, comparamos el resultado con contourf y scatter!
-"""
-#Variables de estado coordenadas
-x0 = xx[zz<240]
-y0 = yy[zz<240]
-z0 = zz[zz<240]/256.
-#Variable de estado: color
-col = plt.get_cmap("viridis")(np.array(0.1+z0))
-
-fig = plt.figure(figsize=(5,5))
-ax = fig.add_subplot(1, 2, 1)
-ax.set_title("x, y, z")
-plt.contourf(x,y,z,cmap = plt.cm.get_cmap('viridis'), levels=np.arange(0,240,2))
-ax = fig.add_subplot(1, 2, 2)
-ax.set_title("x0, y0")
-plt.scatter(x0,y0,c=col,s=0.1)
-plt.show()
-
-
-
-def animate(t, ax):
-    M = np.array([[1,0,0],[0,1,0],[0,0,1]])
-    v=np.array([40,40,0])*t
+def plot_frame_2(t, X0, Y0, Z0, theta, traslacion, ax):
     
-    ax.clear()
-    ax.set_xlim(0,400)
-    ax.set_ylim(0,400)
+    # transformar los datos
+    X, Y, Z = Rotacion(X0, Y0, Z0, theta[t])
+    X, Y, Z = Traslacion(X, Y, Z, traslacion[t])
 
-    X, Y, Z = transf1D(x0, y0, z0, M=M, v=v)
-    col = plt.get_cmap("viridis")(np.array(0.1+z0))
+    # limpiar el gráfico
+    ax.clear()
+    ax.set_xlim(-400,400)
+    ax.set_ylim(-400,400)
+
+    # plot
+    col = plt.get_cmap("viridis")(np.array(0.1+Z0))
     ax.scatter(X,Y,c=col,s=0.1)#,animated=True)
 
+def create_animation_2(X0, Y0, Z0, theta, traslacion):
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(111, projection='3d', xlim=(0,400), ylim=(0,400))
+    ax.view_init(elev=20, azim=-45)
+    ax.set_title("Rotación de la imgen")
+    ani = animation.FuncAnimation(fig, plot_frame_2, frames=np.arange(0,40), fargs=[X0, Y0, Z0, theta, traslacion, ax], interval=10)
+    return ani
 
-fig = plt.figure(figsize=(6,6))
-ax = fig.add_subplot(121, projection='3d', xlim=(0,400), ylim=(0,400))
-ax.view_init(elev=20, azim=-45)
-ax.set_title("Animation transf1D")
-ani = animation.FuncAnimation(fig, animate, frames=np.arange(0,1,0.025), fargs=[ax], interval=20)
+def problema2():
+    img = io.imread('arbol.png')
+    x0, y0, z0 = get_xyz(img)
+    N = 50
+    d = x0.max() - x0.min()
+    theta = np.linspace(0, 3*np.pi, N)
+    traslacion = np.linspace(0, d, N)
+    ani = create_animation_2(x0, y0, z0, theta, traslacion)
+    plt.show()
 #os.chdir()
 #ani.save("p4ii.gif", fps = 10)  
 #os.getcwd()
@@ -177,4 +135,5 @@ ani = animation.FuncAnimation(fig, animate, frames=np.arange(0,1,0.025), fargs=[
 
 
 if __name__ == "__main__":
-    problema1()
+    #problema1()
+    problema2()
